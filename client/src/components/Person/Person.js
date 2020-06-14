e8e716cfaf88cf8dc53a5020fb2529a65f767062
Ildana <ildana.ruzybayeva@gmail.com>
2020-06-14 11:10:06 +0000

    Styled person component with icon and hover animations

--- client/src/components/Person/Person.js
+++ client/src/components/Person/Person.js
@@ -12,35 +12,37 @@ function Person(props) {
   return (
     <div className="person">
       <img src={props.img} alt={props.name} />
-      <div className="person-name">
-        <h1>{props.name}</h1>
-        <h4>Office: {props.office}</h4>
-      </div>
+      <div className="person-details">
+        <div className="person-name">
+          <h1>{props.name}</h1>
+          <h4>Office: {props.office}</h4>
+        </div>
 
-      <div className="person-social">
-        {props.twitter !== null ? (
-          <a href={`https://twitter.com/search?q=${props.twitter}`}>
-            <img src={twitterIcon} style={iconStyle} alt="icon" />
-          </a>
-        ) : (
-          <img src={twitterIcon} style={{ display: "none" }} alt="icon" />
-        )}
+        <div className="person-social">
+          {props.twitter !== null ? (
+            <a href={`https://twitter.com/search?q=${props.twitter}`}>
+              <img src={twitterIcon} style={iconStyle} alt="icon" />
+            </a>
+          ) : (
+            <img src={twitterIcon} style={{ display: "none" }} alt="icon" />
+          )}
 
-        {props.github !== null ? (
-          <a href={`https://github.com/${props.github}`}>
-            <img src={githubIcon} style={iconStyle} alt="icon" />
-          </a>
-        ) : (
-          <img src={githubIcon} style={{ display: "none" }} alt="icon" />
-        )}
+          {props.github !== null ? (
+            <a href={`https://github.com/${props.github}`}>
+              <img src={githubIcon} style={iconStyle} alt="icon" />
+            </a>
+          ) : (
+            <img src={githubIcon} style={{ display: "none" }} alt="icon" />
+          )}
 
-        {props.linkedin !== null ? (
-          <a href={`https://www.linkedin.com/${props.linkedin}`}>
-            <img src={linkedinIcon} style={iconStyle} alt="icon" />
-          </a>
-        ) : (
-          <img src={linkedinIcon} style={{ display: "none" }} alt="icon" />
-        )}
+          {props.linkedin !== null ? (
+            <a href={`https://www.linkedin.com/${props.linkedin}`}>
+              <img src={linkedinIcon} style={iconStyle} alt="icon" />
+            </a>
+          ) : (
+            <img src={linkedinIcon} style={{ display: "none" }} alt="icon" />
+          )}
+        </div>
       </div>
     </div>
   );
