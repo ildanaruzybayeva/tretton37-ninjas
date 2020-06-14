2836ff5e80114b7c11f1528c7e4f165369c927ca
Ildana <ildana.ruzybayeva@gmail.com>
2020-06-14 11:24:03 +0000

    Moved icon styling to css file

--- client/src/components/Person/Person.js
+++ client/src/components/Person/Person.js
@@ -3,25 +3,19 @@ import "./Person.css";
 import twitterIcon from "./icons/twitterIcon.png";
 import githubIcon from "./icons/githubIcon.png";
 import linkedinIcon from "./icons/linkedinIcon.png";
-const iconStyle = {
-  width: "40px",
-  height: "40px"
-};
-
 function Person(props) {
   return (
     <div className="person">
       <img src={props.img} alt={props.name} />
       <div className="person-details">
         <div className="person-name">
-          <h1>{props.name}</h1>
-          <h4>Office: {props.office}</h4>
+          <p>{props.name}</p>
+          <p>Office: {props.office}</p>
         </div>
-
         <div className="person-social">
           {props.twitter !== null ? (
             <a href={`https://twitter.com/search?q=${props.twitter}`}>
-              <img src={twitterIcon} style={iconStyle} alt="icon" />
+              <img src={twitterIcon} alt="icon" />
             </a>
           ) : (
             <img src={twitterIcon} style={{ display: "none" }} alt="icon" />
@@ -29,7 +23,7 @@ function Person(props) {
 
           {props.github !== null ? (
             <a href={`https://github.com/${props.github}`}>
-              <img src={githubIcon} style={iconStyle} alt="icon" />
+              <img src={githubIcon} alt="icon" />
             </a>
           ) : (
             <img src={githubIcon} style={{ display: "none" }} alt="icon" />
@@ -37,7 +31,7 @@ function Person(props) {
 
           {props.linkedin !== null ? (
             <a href={`https://www.linkedin.com/${props.linkedin}`}>
-              <img src={linkedinIcon} style={iconStyle} alt="icon" />
+              <img src={linkedinIcon} alt="icon" />
             </a>
           ) : (
             <img src={linkedinIcon} style={{ display: "none" }} alt="icon" />
