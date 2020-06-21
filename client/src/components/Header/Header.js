10b7506f078a341971d235bbafc7594772c91030
Ildana <ildana.ruzybayeva@gmail.com>
2020-06-21 17:30:56 +0000

    Moved files and minor code style fixes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
             src="https://uploads-ssl.webflow.com/5dd7a459b9c85c38867d8866/5dd7d825be2670c99e30442d__tretton37_logo_green.png"
             className="logo"
             alt="csgo-icon"
-            onClick={()=> window.location.reload()}
+            onClick={() => window.location.reload()}
           />
         </nav>
       </header>
